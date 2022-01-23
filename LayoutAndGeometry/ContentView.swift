cc626fbc3af1780d1f1d90a8d100ccc312830c57
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-23 15:18:34 +0300

    Day 94 — Make the views change color as the user scrolls

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-    
     var body: some View {
         GeometryReader { fullView in
             ScrollView {
@@ -18,7 +16,7 @@ struct ContentView: View {
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
-                            .background(colors[index % 7])
+                            .background(Color(hue: min(1, (geo.frame(in: .global).minY / fullView.size.height) * 0.9), saturation: 1, brightness: 1))
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .scaleEffect(max(0.5, (geo.frame(in: .global).maxY + fullView.size.height / 2.15) / fullView.size.height - 0.05))
                             .opacity(geo.frame(in: .global).minY <= 200 ? geo.frame(in: .global).minY / 200 - 0.1 : 1)
